adc82b2d6c76a858d62166bdc6e5d1f5639ec968
martina-nu <martinanu98@gmail.com>
2022-05-16 04:23:55 +0000

    uploading my code

--- .learn/resets/07-lambda-function-two/app.py
+++ .learn/resets/07-lambda-function-two/app.py
@@ -1,5 +1,5 @@
 
-rapid= lambda my_string: my_string[:-1]
+
 
 # From this line above, plese do not change code below
 print(rapid("bob")) #should print bo
--- exercises/09-Function-parameters/app.py
+++ exercises/09-Function-parameters/app.py
@@ -1,6 +1,7 @@
 # Your code goes here:
-def render_person(param):
-    return param
+def render_person(name, bday, eye_color, age, gender):
+    string = name + " is a " + str(age) + " years old " + gender + " born in " + bday + " with " + eye_color + " eyes"
+    return string
 
 
 # Do not edit below this line
